1efdada56a4e3ef0c882e8d1687a834d6ac47ac0
lee <77390758+dharana77@users.noreply.github.com>
2024-05-01 19:20:13 +0900

    [level 2] Title: 구명보트, Time: 8.38 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+def solution(people, limit):
+    answer = 0
+    people = sorted(people)
+    
+    tot = 0
+    left, right = 0, len(people)-1
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -=1
+        else:
+            right -=1
+        answer += 1
+    
+    return answer
