c572418df4a80a373ae720cfbf26578f6a720155
dharana <dharana7723@gmail.com>
2024-06-27 10:21:26 +0900

    fix: 정렬 문제 해결

--- makings/boj_2993_미네랄.java
+++ makings/boj_2993_미네랄.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -17,6 +18,26 @@ public class Main {
 
     public static class Cluster{
         private List<Pair> points = new ArrayList<>();
+
+        public void sort() {
+            this.points.sort(Comparator.comparingInt(p -> -p.getX()));
+        }
+
+        public boolean isFlying(){
+            int cx = this.points.get(0).x;
+            int cy = this.points.get(0).y;
+            if(cx == R-1){
+                System.out.println("first");
+                return false;
+            }else if(cy + 1 < C && arr[cx][cy+1] == 'x') {
+                System.out.println("second" + cx + " " + cy);
+                return false;
+            }else if(cy - 1 >= 0 && arr[cx][cy-1] == 'x'){
+                System.out.println("third");
+                return false;
+            }
+            return true;
+        }
     }
 
     public static class Pair{
@@ -25,6 +46,10 @@ public class Main {
         public Pair(int x, int y){
             this.x = x;
             this.y = y;
+        }
+
+        public int getX(){
+            return this.x;
         }
     }
 
@@ -39,7 +64,6 @@ public class Main {
 
         for(int i=0; i<R; i++){
             st = new StringTokenizer(br.readLine());
-//            System.out.println(st);
             String line = st.nextToken();
             System.out.println(line);
             for(int j=0; j<C; j++){
@@ -56,9 +80,10 @@ public class Main {
             }else{
                 startFromRight(h);
             }
-            List<Cluster> clusters = findFlyingClusters();
-            //cluster에 minHeight 추가 및 minHeight 기준으로 정렬해야 할듯
-            //그래야 클러스터 여러개 인데 다른 클러스터에 부딪쳐서 우선순위가 제대로 처리안될때 생기는 오류 수정할 수 있을듯
+            boolean[][] visited = new boolean[R][C];
+            List<Cluster> clusters = findFlyingClusters(visited);
+            //cluster를 높이 기준으로 정렬해서 저장하고, 클러스터 모음도 높이가 가장 낮은 것 기준으로 정렬해서 높이가 낮은 클러스터부터 중력을 적용시킨다.
+            //그래야 클러스터 여러개 인데 다른 클러스터에 부딪쳐서 우선순위가 제대로 처리안될때 생기는 오류 수정할 수 있을 것으로 보인다.
             for (int k=0; k< clusters.size(); k++){
                 applyGravityForClusters(clusters.get(k));
             }
@@ -92,16 +117,40 @@ public class Main {
         }
     }
 
-    public static List<Cluster> findFlyingClusters(){
-        boolean[][] visited = new boolean[R][C];
+    public static List<Cluster> findFlyingClusters(boolean[][] visited){
         List<Cluster> clusters = new ArrayList<>();
         for(int i=0; i<R; i++){
             for(int j=0; j<C; j++){
-                if(arr[i][j] == 'x' && !visited[i][j]){
+                if(!visited[i][j] && arr[i][j] == 'x'){
+                    visited[i][j] = true;
                     Cluster cluster = new Cluster();
                     dfs(i, j, visited, cluster);
-                    clusters.add(cluster);
+//                    System.out.println("before sorted");
+                    for(int k=0; k<cluster.points.size(); k++){
+                        Pair p = cluster.points.get(k);
+                        System.out.println(p.x + " " + p.y);
+                    }
+                    cluster.sort();
+//                    System.out.println("after sorted");
+                    for(int k=0; k<cluster.points.size(); k++){
+                        Pair p = cluster.points.get(k);
+                        System.out.println(p.x + " " + p.y);
+                    }
+//                    System.out.println("=========");
+//                    System.out.println(cluster.points.get(0).x + " " + cluster.points.get(0).y);
+                    if(cluster.isFlying()) {
+                        clusters.add(cluster);
+                    }
                 }
+            }
+        }
+        clusters.sort(Comparator.comparingInt(cluster -> cluster.points.get(0).x));
+
+        for(int i=0; i < clusters.size(); i++){
+            Cluster cluster = clusters.get(i);
+            for(int j=0; j<cluster.points.size(); j++){
+                Pair p = cluster.points.get(j);
+                System.out.println(p.x + " " + p.y);
             }
         }
         return clusters;
@@ -116,18 +165,24 @@ public class Main {
             int ny = y + dy[i];
             if(nx >= 0 && nx < R && ny >= 0 && ny < C){
                 if(arr[nx][ny] == 'x' && !visited[nx][ny]){
+                    visited[nx][ny] = true;
                     dfs(nx, ny, visited, cluster);
                 }
             }
         }
     }
 
-    public static void applyGravityForClusters(Cluster cluster){
-        for(int j=0; j<C; j++){
-            for(int i=R-1; i>=0; i--){
-                if(i-1 >=0 && arr[i-1][j] == 'x' && arr[i][j] == '.'){
-                    arr[i][j] = 'x';
-                    arr[i-1][j] = '.';
+    public static void applyGravityForClusters(Cluster cluster) {
+        List<Pair> points = cluster.points;
+        for (int k = 0; k < points.size(); k++) {
+            Pair p = points.get(k);
+            int x = p.x;
+            int y = p.y;
+
+            for (int i = x; i < R; i++) {
+                if (i + 1 < (R - 1) && arr[i][y] == 'x' && arr[i+1][y] == '.') {
+                    arr[i+1][y] = 'x';
+                    arr[i][y] = '.';
                 }
             }
         }
